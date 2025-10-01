17651d0f09b953ba7bc259254cd1803a0de1c25f
Norahfalharbi <s202249220@kfupm.edu.sa>
2025-10-01 13:48:01 +0300

    DONE

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import StudentCard from './components/StudentCard'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
 
       <main className="dashboard-main">
         <div className="cards-container">
-          {/* TODO: Import and render StudentCard components here */}
+          <StudentCard name="Norah ALharbi" id="S12345" dept="Computer Science" />
+          <StudentCard name="Fahad ALharbi" id="S67890" dept="Mathematics" />
         </div>
       </main>
     </div>
